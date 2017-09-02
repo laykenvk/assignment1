ba14a26944db2e5b4d7306ccf65c5b3b94fe3bb9
Layken <31434953+laykenvk@users.noreply.github.com>
2017-09-02 07:20:59 +0200

    Create logged-in.php

---
+++ logged-in.php
@@ -0,0 +1,30 @@
+<?php
+session_start();
+?>
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<link href="style.css" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Fjalla+One" rel="stylesheet">
+<title>Logged In</title>
+</head>
+<body>
+    <h1 id="title">Welcome</h1>
+    <?php
+    if (!empty($_SESSION['uid'])){
+		echo 'Logged in as user '.$_SESSION['un'];
+		echo '<br> This is your own secret page!';
+	}
+	else {
+		echo 'Not logged in...';
+	}
+    
+    ?>
+    
+    <div id="wrapper">
+        <a href="logout.php" style="text-decoration:none; color:black; text-align:center;"><p id="button">Logout</p></a>
+    </div>
+    
+</body>
+</html>
